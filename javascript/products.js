a29059ccaf74bfec4a01fc0f00ee0fc4ebcf9955
Bobb-93 <borisiakimov@gmail.com>
2021-03-23 16:15:42 +0200

    Add to Cart button style added

--- javascript/products.js
+++ javascript/products.js
@@ -18,22 +18,24 @@ const furnitureTypes = {
     leftDiv: document.getElementById("leftDiv"),
     centerDiv: document.getElementById("centerDiv"),
     rightDiv: document.getElementById("rightDiv"),
-    
 };
 
 const threeDivs = {
     leftProductName: document.querySelector("#leftDiv .productName"),
     leftProductPrice: document.querySelector("#leftDiv .price span"),
+    leftDataPrice: document.querySelector("#leftDiv .addToCart"),
     leftProductDescription: document.querySelector("#leftDiv .description"),
     leftProductImg: document.getElementById("img-1"),
     
     centerProductName: document.querySelector("#centerDiv .productName"),
     centerProductPrice: document.querySelector("#centerDiv .price span"),
+    centerDataPrice: document.querySelector("#centerDiv .addToCart"),
     centerProductDescription: document.querySelector("#centerDiv .description"),
     centerProductImg: document.getElementById("img-2"),
     
     rightProductName: document.querySelector("#rightDiv .productName"),
     rightProductPrice: document.querySelector("#rightDiv .price span"),
+    rightDataPrice: document.querySelector("#rightDiv .addToCart"),
     rightProductDescription: document.querySelector("#rightDiv .description"),
     rightProductImg: document.getElementById("img-3")
     
@@ -315,6 +317,9 @@ const bookcasesList = [
     }
 ];
 
+let totalSum = 0;
+let cart = [];
+
 /*
     Functions
 */
@@ -333,16 +338,19 @@ const loadSofas = function(){
     
     threeDivs.leftProductName.innerText = sofasList[0].name;
     threeDivs.leftProductPrice.innerText = sofasList[0].price;
+    threeDivs.leftDataPrice.setAttribute("data-price", sofasList[0].price);
     threeDivs.leftProductDescription.innerText = sofasList[0].description;
     threeDivs.leftProductImg.src = sofasList[0].imageSrc;
     
     threeDivs.centerProductName.innerText = sofasList[1].name;
     threeDivs.centerProductPrice.innerText = sofasList[1].price;
+    threeDivs.centerDataPrice.setAttribute("data-price", sofasList[1].price);
     threeDivs.centerProductDescription.innerText = sofasList[1].description;
     threeDivs.centerProductImg.src = sofasList[1].imageSrc;
     
     threeDivs.rightProductName.innerText = sofasList[2].name;
     threeDivs.rightProductPrice.innerText = sofasList[2].price;
+    threeDivs.rightDataPrice.setAttribute("data-price", sofasList[2].price);
     threeDivs.rightProductDescription.innerText = sofasList[2].description;
     threeDivs.rightProductImg.src = sofasList[2].imageSrc;
     
@@ -353,16 +361,19 @@ const loadSectionalSofas = function(){
     
     threeDivs.leftProductName.innerText = sectionalSofasList[0].name;
     threeDivs.leftProductPrice.innerText = sectionalSofasList[0].price;
+    threeDivs.leftDataPrice.setAttribute("data-price", sectionalSofasList[0].price);
     threeDivs.leftProductDescription.innerText = sectionalSofasList[0].description;
     threeDivs.leftProductImg.src = sectionalSofasList[0].imageSrc;
     
     threeDivs.centerProductName.innerText = sectionalSofasList[1].name;
     threeDivs.centerProductPrice.innerText = sectionalSofasList[1].price;
+    threeDivs.centerDataPrice.setAttribute("data-price", sectionalSofasList[1].price);
     threeDivs.centerProductDescription.innerText = sectionalSofasList[1].description;
     threeDivs.centerProductImg.src = sectionalSofasList[1].imageSrc;
     
     threeDivs.rightProductName.innerText = sectionalSofasList[2].name;
     threeDivs.rightProductPrice.innerText = sectionalSofasList[2].price;
+    threeDivs.rightDataPrice.setAttribute("data-price", sectionalSofasList[2].price);
     threeDivs.rightProductDescription.innerText = sectionalSofasList[2].description;
     threeDivs.rightProductImg.src = sectionalSofasList[2].imageSrc;
     
@@ -373,16 +384,19 @@ const loadSleeperSofas = function(){
     
     threeDivs.leftProductName.innerText = sleeperSofasList[0].name;
     threeDivs.leftProductPrice.innerText = sleeperSofasList[0].price;
+    threeDivs.leftDataPrice.setAttribute("data-price", sleeperSofasList[0].price);
     threeDivs.leftProductDescription.innerText = sleeperSofasList[0].description;
     threeDivs.leftProductImg.src = sleeperSofasList[0].imageSrc;
     
     threeDivs.centerProductName.innerText = sleeperSofasList[1].name;
     threeDivs.centerProductPrice.innerText = sleeperSofasList[1].price;
+    threeDivs.centerDataPrice.setAttribute("data-price", sleeperSofasList[1].price);
     threeDivs.centerProductDescription.innerText = sleeperSofasList[1].description;
     threeDivs.centerProductImg.src = sleeperSofasList[1].imageSrc;
     
     threeDivs.rightProductName.innerText = sleeperSofasList[2].name;
     threeDivs.rightProductPrice.innerText = sleeperSofasList[2].price;
+    threeDivs.rightDataPrice.setAttribute("data-price", sleeperSofasList[2].price);
     threeDivs.rightProductDescription.innerText = sleeperSofasList[2].description;
     threeDivs.rightProductImg.src = sleeperSofasList[2].imageSrc;
     
@@ -393,16 +407,19 @@ const loadOttomanChairs = function(){
     
     threeDivs.leftProductName.innerText = ottomanChairsList[0].name;
     threeDivs.leftProductPrice.innerText = ottomanChairsList[0].price;
+    threeDivs.leftDataPrice.setAttribute("data-price", ottomanChairsList[0].price);
     threeDivs.leftProductDescription.innerText = ottomanChairsList[0].description;
     threeDivs.leftProductImg.src = ottomanChairsList[0].imageSrc;
     
     threeDivs.centerProductName.innerText = ottomanChairsList[1].name;
     threeDivs.centerProductPrice.innerText = ottomanChairsList[1].price;
+    threeDivs.centerDataPrice.setAttribute("data-price", ottomanChairsList[1].price);
     threeDivs.centerProductDescription.innerText = ottomanChairsList[1].description;
     threeDivs.centerProductImg.src = ottomanChairsList[1].imageSrc;
     
     threeDivs.rightProductName.innerText = ottomanChairsList[2].name;
     threeDivs.rightProductPrice.innerText = ottomanChairsList[2].price;
+    threeDivs.rightDataPrice.setAttribute("data-price", ottomanChairsList[2].price);
     threeDivs.rightProductDescription.innerText = ottomanChairsList[2].description;
     threeDivs.rightProductImg.src = ottomanChairsList[2].imageSrc;
     
@@ -413,16 +430,19 @@ const loadDiningTables = function(){
     
     threeDivs.leftProductName.innerText = diningTablesList[0].name;
     threeDivs.leftProductPrice.innerText = diningTablesList[0].price;
+    threeDivs.leftDataPrice.setAttribute("data-price", diningTablesList[0].price);
     threeDivs.leftProductDescription.innerText = diningTablesList[0].description;
     threeDivs.leftProductImg.src = diningTablesList[0].imageSrc;
     
     threeDivs.centerProductName.innerText = diningTablesList[1].name;
     threeDivs.centerProductPrice.innerText = diningTablesList[1].price;
+    threeDivs.centerDataPrice.setAttribute("data-price", diningTablesList[1].price);
     threeDivs.centerProductDescription.innerText = diningTablesList[1].description;
     threeDivs.centerProductImg.src = diningTablesList[1].imageSrc;
     
     threeDivs.rightProductName.innerText = diningTablesList[2].name;
     threeDivs.rightProductPrice.innerText = diningTablesList[2].price;
+    threeDivs.rightDataPrice.setAttribute("data-price", diningTablesList[2].price);
     threeDivs.rightProductDescription.innerText = diningTablesList[2].description;
     threeDivs.rightProductImg.src = diningTablesList[2].imageSrc;
     
@@ -433,16 +453,19 @@ const loadArmChairs = function(){
     
     threeDivs.leftProductName.innerText = armChairsList[0].name;
     threeDivs.leftProductPrice.innerText = armChairsList[0].price;
+    threeDivs.leftDataPrice.setAttribute("data-price", armChairsList[0].price);
     threeDivs.leftProductDescription.innerText = armChairsList[0].description;
     threeDivs.leftProductImg.src = armChairsList[0].imageSrc;
     
     threeDivs.centerProductName.innerText = armChairsList[1].name;
     threeDivs.centerProductPrice.innerText = armChairsList[1].price;
+    threeDivs.centerDataPrice.setAttribute("data-price", armChairsList[1].price);
     threeDivs.centerProductDescription.innerText = armChairsList[1].description;
     threeDivs.centerProductImg.src = armChairsList[1].imageSrc;
     
     threeDivs.rightProductName.innerText = armChairsList[2].name;
     threeDivs.rightProductPrice.innerText = armChairsList[2].price;
+    threeDivs.rightDataPrice.setAttribute("data-price", armChairsList[2].price);
     threeDivs.rightProductDescription.innerText = armChairsList[2].description;
     threeDivs.rightProductImg.src = armChairsList[2].imageSrc;
     
@@ -453,16 +476,19 @@ const loadConsoleTables = function(){
     
     threeDivs.leftProductName.innerText = consoleTablesList[0].name;
     threeDivs.leftProductPrice.innerText = consoleTablesList[0].price;
+    threeDivs.leftDataPrice.setAttribute("data-price", consoleTablesList[0].price);
     threeDivs.leftProductDescription.innerText = consoleTablesList[0].description;
     threeDivs.leftProductImg.src = consoleTablesList[0].imageSrc;
     
     threeDivs.centerProductName.innerText = consoleTablesList[1].name;
     threeDivs.centerProductPrice.innerText = consoleTablesList[1].price;
+    threeDivs.centerDataPrice.setAttribute("data-price", consoleTablesList[1].price);
     threeDivs.centerProductDescription.innerText = consoleTablesList[1].description;
     threeDivs.centerProductImg.src = consoleTablesList[1].imageSrc;
     
     threeDivs.rightProductName.innerText = consoleTablesList[2].name;
     threeDivs.rightProductPrice.innerText = consoleTablesList[2].price;
+    threeDivs.rightDataPrice.setAttribute("data-price", consoleTablesList[2].price);
     threeDivs.rightProductDescription.innerText = consoleTablesList[2].description;
     threeDivs.rightProductImg.src = consoleTablesList[2].imageSrc;
     
@@ -473,16 +499,19 @@ const loadBeds = function(){
     
     threeDivs.leftProductName.innerText = bedsList[0].name;
     threeDivs.leftProductPrice.innerText = bedsList[0].price;
+    threeDivs.leftDataPrice.setAttribute("data-price", bedsList[0].price);
     threeDivs.leftProductDescription.innerText = bedsList[0].description;
     threeDivs.leftProductImg.src = bedsList[0].imageSrc;
     
     threeDivs.centerProductName.innerText = bedsList[1].name;
     threeDivs.centerProductPrice.innerText = bedsList[1].price;
+    threeDivs.centerDataPrice.setAttribute("data-price", bedsList[1].price);
     threeDivs.centerProductDescription.innerText = bedsList[1].description;
     threeDivs.centerProductImg.src = bedsList[1].imageSrc;
     
     threeDivs.rightProductName.innerText = bedsList[2].name;
     threeDivs.rightProductPrice.innerText = bedsList[2].price;
+    threeDivs.rightDataPrice.setAttribute("data-price", bedsList[2].price);
     threeDivs.rightProductDescription.innerText = bedsList[2].description;
     threeDivs.rightProductImg.src = bedsList[2].imageSrc;
     
@@ -493,16 +522,19 @@ const loadNightstands = function(){
     
     threeDivs.leftProductName.innerText = nightstandsList[0].name;
     threeDivs.leftProductPrice.innerText = nightstandsList[0].price;
+    threeDivs.leftDataPrice.setAttribute("data-price", nightstandsList[0].price);
     threeDivs.leftProductDescription.innerText = nightstandsList[0].description;
     threeDivs.leftProductImg.src = nightstandsList[0].imageSrc;
     
     threeDivs.centerProductName.innerText = nightstandsList[1].name;
     threeDivs.centerProductPrice.innerText = nightstandsList[1].price;
+    threeDivs.centerDataPrice.setAttribute("data-price", nightstandsList[1].price);
     threeDivs.centerProductDescription.innerText = nightstandsList[1].description;
     threeDivs.centerProductImg.src = nightstandsList[1].imageSrc;
     
     threeDivs.rightProductName.innerText = nightstandsList[2].name;
     threeDivs.rightProductPrice.innerText = nightstandsList[2].price;
+    threeDivs.rightDataPrice.setAttribute("data-price", nightstandsList[2].price);
     threeDivs.rightProductDescription.innerText = nightstandsList[2].description;
     threeDivs.rightProductImg.src = nightstandsList[2].imageSrc;
     
@@ -513,16 +545,19 @@ const loadMirrors = function(){
     
     threeDivs.leftProductName.innerText = mirrorsList[0].name;
     threeDivs.leftProductPrice.innerText = mirrorsList[0].price;
+    threeDivs.leftDataPrice.setAttribute("data-price", mirrorsList[0].price);
     threeDivs.leftProductDescription.innerText = mirrorsList[0].description;
     threeDivs.leftProductImg.src = mirrorsList[0].imageSrc;
     
     threeDivs.centerProductName.innerText = mirrorsList[1].name;
     threeDivs.centerProductPrice.innerText = mirrorsList[1].price;
+    threeDivs.centerDataPrice.setAttribute("data-price", mirrorsList[1].price);
     threeDivs.centerProductDescription.innerText = mirrorsList[1].description;
     threeDivs.centerProductImg.src = mirrorsList[1].imageSrc;
     
     threeDivs.rightProductName.innerText = mirrorsList[2].name;
     threeDivs.rightProductPrice.innerText = mirrorsList[2].price;
+    threeDivs.rightDataPrice.setAttribute("data-price", mirrorsList[2].price);
     threeDivs.rightProductDescription.innerText = mirrorsList[2].description;
     threeDivs.rightProductImg.src = mirrorsList[2].imageSrc;
     
@@ -533,16 +568,19 @@ const loadStorageCabinets = function(){
     
     threeDivs.leftProductName.innerText = storageCabinetsList[0].name;
     threeDivs.leftProductPrice.innerText = storageCabinetsList[0].price;
+    threeDivs.leftDataPrice.setAttribute("data-price", storageCabinetsList[0].price);
     threeDivs.leftProductDescription.innerText = storageCabinetsList[0].description;
     threeDivs.leftProductImg.src = storageCabinetsList[0].imageSrc;
     
     threeDivs.centerProductName.innerText = storageCabinetsList[1].name;
     threeDivs.centerProductPrice.innerText = storageCabinetsList[1].price;
+    threeDivs.centerDataPrice.setAttribute("data-price", storageCabinetsList[1].price);
     threeDivs.centerProductDescription.innerText = storageCabinetsList[1].description;
     threeDivs.centerProductImg.src = storageCabinetsList[1].imageSrc;
     
     threeDivs.rightProductName.innerText = storageCabinetsList[2].name;
     threeDivs.rightProductPrice.innerText = storageCabinetsList[2].price;
+    threeDivs.rightDataPrice.setAttribute("data-price", storageCabinetsList[2].price);
     threeDivs.rightProductDescription.innerText = storageCabinetsList[2].description;
     threeDivs.rightProductImg.src = storageCabinetsList[2].imageSrc;
     
@@ -553,16 +591,19 @@ const loadDesks = function(){
     
     threeDivs.leftProductName.innerText = desksList[0].name;
     threeDivs.leftProductPrice.innerText = desksList[0].price;
+    threeDivs.leftDataPrice.setAttribute("data-price", desksList[0].price);
     threeDivs.leftProductDescription.innerText = desksList[0].description;
     threeDivs.leftProductImg.src = desksList[0].imageSrc;
     
     threeDivs.centerProductName.innerText = desksList[1].name;
     threeDivs.centerProductPrice.innerText = desksList[1].price;
+    threeDivs.centerDataPrice.setAttribute("data-price", desksList[1].price);
     threeDivs.centerProductDescription.innerText = desksList[1].description;
     threeDivs.centerProductImg.src = desksList[1].imageSrc;
     
     threeDivs.rightProductName.innerText = desksList[2].name;
     threeDivs.rightProductPrice.innerText = desksList[2].price;
+    threeDivs.rightDataPrice.setAttribute("data-price", desksList[2].price);
     threeDivs.rightProductDescription.innerText = desksList[2].description;
     threeDivs.rightProductImg.src = desksList[2].imageSrc;
     
@@ -573,16 +614,19 @@ const loadBookcases = function(){
     
     threeDivs.leftProductName.innerText = bookcasesList[0].name;
     threeDivs.leftProductPrice.innerText = bookcasesList[0].price;
+    threeDivs.leftDataPrice.setAttribute("data-price", bookcasesList[0].price);
     threeDivs.leftProductDescription.innerText = bookcasesList[0].description;
     threeDivs.leftProductImg.src = bookcasesList[0].imageSrc;
     
     threeDivs.centerProductName.innerText = bookcasesList[1].name;
     threeDivs.centerProductPrice.innerText = bookcasesList[1].price;
+    threeDivs.centerDataPrice.setAttribute("data-price", bookcasesList[1].price);
     threeDivs.centerProductDescription.innerText = bookcasesList[1].description;
     threeDivs.centerProductImg.src = bookcasesList[1].imageSrc;
     
     threeDivs.rightProductName.innerText = bookcasesList[2].name;
     threeDivs.rightProductPrice.innerText = bookcasesList[2].price;
+    threeDivs.rightDataPrice.setAttribute("data-price", bookcasesList[2].price);
     threeDivs.rightProductDescription.innerText = bookcasesList[2].description;
     threeDivs.rightProductImg.src = bookcasesList[2].imageSrc;
     
